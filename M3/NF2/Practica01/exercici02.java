1f7355b09befd7300300f1eb174efac3037580fc
lapineda201920 <55738598+lapineda201920@users.noreply.github.com>
2019-10-27 16:26:04 +0100

    exercici01

--- M3/NF2/Practica01/exercici02.java
+++ M3/NF2/Practica01/exercici02.java
@@ -15,24 +15,34 @@ class Main {
     return num;
   }
 
+  public static int multiplicar(int num1, int num2, int mult){
+    mult = num1 * num2;
+    return mult;
+  }
+
+  public static int sumar(int num1, int num2, int suma){
+    suma = num1 + num2;
+    return suma;
+  }
+
   public static void main(String[] args) {
     Scanner sc=new Scanner(System.in);
     System.out.print("Introdueix un numero: ");
     int num1=sc.nextInt();
     System.out.print("Introdueix un altre numero: ");
     int num2=sc.nextInt();
-    int num=1;
+    int num=0, suma=0, mult=0;
 
     while (num!=3){
       num=menu(num);
       switch (num) {
         case 1:
-          int suma= num1+num2;
-          System.out.println("Suma:"+num1+"+"+num2+"="+suma);
+          suma= sumar(num1,num2,suma);
+          System.out.println("Suma:"+suma);
           break;
         case 2:
-          int mult= num1*num2;
-          System.out.println("Multiplicació:"+num1+"*"+num2+"="+mult);
+          mult= multiplicar(num1,num2,suma);
+          System.out.println("Multiplicació:"+mult);
           break;
         case 3:
           System.out.println("Tancant el programa");
--- M3/NF2/Practica01/exercici03.java
+++ M3/NF2/Practica01/exercici03.java
@@ -1,34 +1,94 @@
 import java.util.Scanner;
 
-class Main {
-  public static void main(String[] args) {
-    Scanner sc=new Scanner(System.in);
-    System.out.print("Introdueix un numero: ");
-    int num1=sc.nextInt();
-    System.out.print("Introdueix un altre numero: ");
-    int num2=sc.nextInt();
-    int num=1;
+class cuenta_bancaria {
+  String nombre, constructor;
+  int balance=0;
 
-    while (num!=3){
-      System.out.print("\n-----MENU-----\n");
-      System.out.print("\nQuè vols fer:");
-      System.out.print("\n1) Sumar");
-      System.out.print("\n2) Multiplicar");
-      System.out.print("\n3) Sortir\n\n");
-      System.out.print("Resposta: ");
-      num=sc.nextInt();
+  cuenta_bancaria (String nombre, String constructor, int balance){
+    this.nombre=nombre;      
+    this.constructor=constructor;      
+    this.balance=balance;
+  }
+
+  public void depositarDinero(){
+    Scanner sc=new Scanner(System.in);
+    int depositar=0;
+
+    System.out.print("Introdueix la quantitat a depositar: ");
+    depositar=sc.nextInt();
+
+    balance=depositar+balance;
+
+    System.out.print("Total del balanç: "+balance);
+  }
+
+  public void retirarDinero(){
+    Scanner sc=new Scanner(System.in);
+    int retirar=0;
+
+    System.out.print("Introdueix la quantitat a retirar: ");
+    retirar=sc.nextInt();
+
+    balance=balance-retirar;
+
+    System.out.print("Total del balanç: "+balance);
+  }
+
+  public void obtenerBalance(){
+    System.out.print("El total del balanç és: "+balance);
+  }
+}
+
+class Main {
+
+  public static int menu(int num){
+    Scanner sc=new Scanner(System.in);
+
+    System.out.print("\n\n-----MENU-----\n");
+    System.out.print("\nQuè vols fer:");
+    System.out.print("\n1) Depositar Diners");
+    System.out.print("\n2) Retirar Diners");
+    System.out.print("\n3) Obtenir Balanç");
+    System.out.print("\n4) Tancar Programma\n\n");
+    System.out.print("Resposta: ");
+    num=sc.nextInt();
+    return num;
+  }
+
+  public static void main(String[] args) {
+    String nombre, constructor;
+    int balance=0, num=0;
+
+    Scanner sc=new Scanner(System.in);
+
+    System.out.print("Introdueix el nom de la persona: ");
+    nombre=sc.nextLine();
+    System.out.print("Introdueix el tipus de compte: ");
+    constructor=sc.nextLine();
+    System.out.print("Introdueix el balanç: ");
+    balance=sc.nextInt();
+    
+    cuenta_bancaria usuari=new cuenta_bancaria(nombre,constructor,balance);
+
+    while (num!=4){
+      num=menu(num);
       switch (num) {
         case 1:
-          int suma= num1+num2;
-          System.out.println("Suma:"+num1+"+"+num2+"="+suma);
-          break;
-        case 2:
-          int mult= num1*num2;
-          System.out.println("Multiplicació:"+num1+"*"+num2+"="+mult);
-          break;
-        case 3:
-        System.out.println("Tancant el programa");
+          usuari.depositarDinero();
         break;
+        case 2:
+          usuari.retirarDinero();
+        break;
+        case 3:
+          usuari.obtenerBalance();
+        break;
+        case 4:
+          System.out.println("Tancant el programa");
+        break;
+        default:
+          System.out.println("\nNumero incorrecte!\n");
+        break;
+
       }
     }
   }
